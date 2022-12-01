f577c97a5c33f8a51eb41e110c2a84caa16a31f7
Alex6599 <116551983+Alex6599@users.noreply.github.com>
2022-12-02 00:56:43 +0600

    html,css

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var menu = document.getElementById("menu");
+
+menu.onclick = function () {
+  menu.classList.toggle("openmenu");
+  navigation.style.height = "350px";
+  dd.style.opacity = "1";
+  tt.style.opacity = "1";
+  pp.style.opacity = "1";
+  oo.style.opacity = "1";
+  rr.style.opacity = "1";
+};
